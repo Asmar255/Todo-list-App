1dd2747de2e7cd262db7594afae6e6df176d2f16
Asmar255 <asmarnaeem255@gmail.com>
2026-08-28 17:38:31 +0500

    done craeting the initial Ui

--- Todo-list/app/(tabs)/index.tsx
+++ Todo-list/app/(tabs)/index.tsx
@@ -1,7 +1,8 @@
-import { Pressable, StyleSheet,Text,TextInput,View, } from "react-native";
+import { Pressable, StyleSheet,Text,TextInput,View,FlatList,Modal } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { Ionicons } from '@expo/vector-icons';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function HomeScreen() {
   return (
@@ -13,23 +14,70 @@ export default function HomeScreen() {
       </View>
 
       {/* Input Section  */}
-      <View>
+      <View style={styles.inputConatiner}>
         <TextInput
+        style={styles.input}
         placeholder="Add new task..."
         placeholderTextColor="#888"
         />
+
+        <Pressable style={styles.TimePicker}>
+          <Text>hello</Text>
+        </Pressable>
+
+        <Pressable style={styles.plusBtn}>
+          <Text style={styles.plusBtnText}>+</Text>
+        </Pressable>
       </View>
 
-      {/* cards section */}
-      <View>
-        
-        {/* SubCards */}
+      {/* FlatList Section  */}
+      <View style={styles.cardContainer}>
+          <Pressable>
+            <AntDesign name="check-circle" size={24} color="black" />       
+          </Pressable>
 
-        <View>
-            
+          <View style={styles.Todotxtpart}>
+            <Text style={styles.todoTxt}>hello</Text>
+            <Text style={styles.todoTxt}>🕒 17:24</Text>
+          </View>
+
+          <View style={styles.actionBtn}>
+            <Pressable>
+              <Ionicons name="create-outline" size={20} color="#BC8F8F" />
+            </Pressable>
+            <Pressable>
+              <Ionicons name="trash-outline" size={20} color="#BC8F8F" />
+            </Pressable>
+          </View>
+
+      </View>
+
+      {/* Modal for edit  */}
+      <Modal  animationType="fade">
+        <View style={styles.Modal}>
+          <View style={styles.ModalContent}>
+              <Text style={styles.heading}>Edit Task</Text>
+
+              <Text style={styles.subHeading}>Task Name</Text>
+              <TextInput
+              style={styles.inputModal}
+              
+              />
+
+              <Text style={styles.subHeading}>Time</Text>
+              <Pressable>
+                <Text style={styles.textTime}>🕒</Text>
+              </Pressable>
+
+              <View style={styles.modalBtnContainer}>
+                <Text style={styles.modalBtnCancel}>Cancel</Text>
+                <Text style={styles.modalBtn}>Save</Text>
+              </View>
+
+          </View>
+
         </View>
-
-      </View>
+      </Modal>
 
     </SafeAreaView>
   );
@@ -38,18 +86,120 @@ export default function HomeScreen() {
 const styles=StyleSheet.create({
   // Header
   header:{
-    flex:1,
-    justifyContent:'center',
     alignItems:'center',
     backgroundColor:'#2F4F4F'
   },
   headerText:{
     color:'#fff',
-    fontSize:40,
+    fontSize:30,
     fontWeight:'bold',
     padding:20,
   },
 
-  //Cards
- 
+  //input section
+  inputConatiner:{
+    flexDirection:'row',
+    alignItems:'center',
+    gap:6,
+    padding: 12,
+  },
+  input:{
+    flex:2,
+    borderColor:'##CCC',
+    borderWidth:1,
+    borderRadius:12,
+  },
+  TimePicker:{
+    backgroundColor:'#bfb0b0',
+    padding:13,
+    borderRadius:12,
+  },
+  plusBtn:{
+    backgroundColor:'#7D5252',
+    padding:10,
+    borderRadius:10,
+    color:'#fff',
+    alignItems:'center',
+    width:50,
+  },
+  plusBtnText:{
+      color:'#fff',
+      fontWeight:'bold',
+      fontSize:25,
+      margin:'auto',
+  },
+
+  //Cards section
+  cardContainer:{
+    flexDirection:'row',
+    backgroundColor:'#2F4F4F',
+    marginHorizontal:10,
+    marginVertical:10,
+    padding:16,
+    borderRadius:10,
+    alignItems:'center',
+    gap:20,
+  },
+  Todotxtpart:{
+    flex:1,
+  },
+  todoTxt:{
+    color:'#fff'
+  },
+  actionBtn:{
+    flexDirection:'row',
+    justifyContent:'flex-end',
+    gap:10,
+    marginLeft:120,
+  },
+
+  //Modal 
+  Modal:{
+    flex:1,
+    backgroundColor:'#00000080',
+    justifyContent:'center',
+    padding:10,
+  },
+  ModalContent:{
+    backgroundColor:'#fff',
+    borderRadius:10,
+    padding:20,
+  },
+  heading:{
+    color:'#000',
+    fontWeight:'bold',
+    fontSize:25,
+  },
+  subHeading:{
+    marginTop:17,
+    fontSize:14,
+    marginBottom:3,
+  },
+  inputModal:{
+    borderWidth:1,
+    borderRadius:5,
+  },
+  textTime:{
+      borderWidth:1,
+      padding:10,
+      borderRadius:5,
+  },
+  modalBtnContainer:{
+    flexDirection:'row',
+    justifyContent:'flex-end',
+    gap:10,
+    marginTop:10,
+  },
+  modalBtn:{
+    backgroundColor:'#2F4F4F',
+    padding:10,
+    borderRadius:10,
+    color:'#fff',
+    fontWeight:'bold'
+  },
+  modalBtnCancel:{
+    padding:10,
+    borderRadius:10,
+    fontWeight:'bold'
+  },
 })
